0a5bf837bd8a5d9f1c792d2819bbd0593dcdc485
Simon Tharby <jinjamail@gmail.com>
2019-05-14 15:21:56 +0100

    recreate rough page layout dynamically with javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("Hello World!");
+
+const title = document.createElement('h1');
+title.innerHTML = "Decadence";
+content.appendChild(title);
+
+const mainImg = document.createElement('img');
+mainImg.src = "img/restaurant.jpg";
+mainImg.alt = "restaurant";
+mainImg.id = "restaurant";
+content.appendChild(mainImg);
+
+const tagContainer = document.createElement('div');
+tagContainer.classList.add('container');
+content.appendChild(tagContainer);
+
+const tagLine = document.createElement('p');
+tagLine.innerHTML = "Surpass your highest expectations, including price.";
+tagContainer.appendChild(tagLine);
