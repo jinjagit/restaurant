cb22cda426ee0e7e4ba248bba38c3fccf7077c3c
Simon Tharby <jinjamail@gmail.com>
2019-05-14 15:53:03 +0100

    recreate rough page layout dynamically with javascript module

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-console.log("Hello World!");
+//console.log("Hello World!");
 
-const title = document.createElement('h1');
-title.innerHTML = "Decadence";
-content.appendChild(title);
+import { homePage } from './homePage'
 
-const mainImg = document.createElement('img');
-mainImg.src = "img/restaurant.jpg";
-mainImg.alt = "restaurant";
-mainImg.id = "restaurant";
-content.appendChild(mainImg);
-
-const tagContainer = document.createElement('div');
-tagContainer.classList.add('container');
-content.appendChild(tagContainer);
-
-const tagLine = document.createElement('p');
-tagLine.innerHTML = "Surpass your highest expectations, including price.";
-tagContainer.appendChild(tagLine);
+homePage();
